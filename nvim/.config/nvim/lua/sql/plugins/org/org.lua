b261c023cc51ecf139e05e4ca46c42bdf88c4a8e
SqLait <deanroman2005@gmail.com>
2024-08-17 20:56:25 +0200

    .

--- nvim/.config/nvim/lua/sql/plugins/org/org.lua
+++ nvim/.config/nvim/lua/sql/plugins/org/org.lua
@@ -3,6 +3,7 @@ return {
   event = 'VeryLazy',
   ft = { 'org' },
   config = function()
+    -- Setup orgmode
     require('orgmode').setup({
       org_agenda_files = '~/Documents/orgfiles/**/*',
       org_default_notes_file = '~/Documents/orgfiles/refile.org',
