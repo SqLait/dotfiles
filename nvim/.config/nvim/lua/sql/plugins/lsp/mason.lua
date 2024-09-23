ecc5cae941c0394a54d519a8f0d35f2c28606637
SqLait <deanroman2005@gmail.com>
2024-09-23 13:39:03 +0200

    Fixed: nvim & Added: Hyprland multimedia support

--- nvim/.config/nvim/lua/sql/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/sql/plugins/lsp/mason.lua
@@ -24,7 +24,7 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "lua_ls", "clangd", "csharp_ls", "tsserver",
+        "lua_ls", "clangd", "csharp_ls", --[["tsserver",]]
         "pylsp", "rust_analyzer", "zls", "elixirls",
         "html", "cssls"
       },
