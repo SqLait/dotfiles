052f645c65c9b14d24605d4081570cd024f0ea7b
SqLait <deanroman2005@gmail.com>
2024-11-12 11:30:21 +0100

    Updated: Fish config

---
+++ fish/.config/fish/env/init.fish
@@ -0,0 +1,2 @@
+oh-my-posh init fish --config $HOME/.config/ohmyposh/zen.toml | source 
+zoxide init --cmd cd fish | source
---
+++ fish/.config/fish/functions/abrv.fish
@@ -0,0 +1,27 @@
+function lg
+    lazygit
+end
+
+function vim
+    nvim
+end
+
+function vi
+    vim
+end
+
+function neofetch
+    fastfetch
+end
+
+function sudo
+    run0 --background=
+end
+
+function ls
+    eza --color=always --long --git --icons=always
+end
+
+function tree
+    eza --tree --color=always --git --icons=always --all
+end
