c84cfdea7e0cb030512add8c7da30961b1823c1f
SqLait <119504721+SqLait@users.noreply.github.com>
2025-02-12 15:32:28 +0100

    FIXED: issue with the colors :>
    I made an oopsie woopsie while doing shit

--- nvim/.config/nvim/lua/sql/plugins/syntax/colorscheme.lua
+++ nvim/.config/nvim/lua/sql/plugins/syntax/colorscheme.lua
@@ -76,34 +76,3 @@ return {
         end,
     }
 }
-
-            -- Custom configuration options
-            require("gruvbox").setup({
-                terminal_colors = true,
-                undercurl = true,
-                underline = true,
-                bold = false,
-                italic = {
-                    strings = false,
-                    emphasis = false,
-                    comments = false,
-                    operators = false,
-                    folds = false,
-                },
-                strikethrough = true,
-                invert_selection = true,
-                invert_signs = false,
-                invert_tabline = false,
-                invert_intend_guides = false,
-                inverse = false,
-                contrast = "", -- "hard", "soft", or ""
-                palette_overrides = {},
-                overrides = {},
-                dim_inactive = false,
-                transparent_mode = false,
-            })
-
-            vim.cmd([[colorscheme gruvbox]])
-        end,
-    }
-}
