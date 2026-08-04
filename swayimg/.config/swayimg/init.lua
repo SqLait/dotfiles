65a64c9f685ae18f07a36404e30feb7b21efc0dc
cyu <cyu@noreply.codeberg.org>
2026-08-04 09:34:15 +0200

    updated: lf and swayimg

--- swayimg/.config/swayimg/init.lua
+++ swayimg/.config/swayimg/init.lua
@@ -8,43 +8,44 @@
 -- 4. /etc/xdg/swayimg/init.lua
 
 -- General config
-swayimg.set_mode("viewer")                -- mode at startup
-swayimg.enable_antialiasing(true)         -- anti-aliasing
-swayimg.enable_decoration(true)           -- window title/buttons/borders
-swayimg.enable_overlay(false)             -- window overlay mode
-swayimg.enable_exif_orientation(true)     -- image orientation by EXIF
-swayimg.set_dnd_button("MouseRight")      -- drag-and-drop mouse button
+swayimg.mode = "viewer"                -- mode at startup
+swayimg.fullscreen = true
+swayimg.antialiasing = true         -- anti-aliasing
+swayimg.decoration = true           -- window title/buttons/borders
+swayimg.overlay = false             -- window overlay mode
+swayimg.exif_orientation = true     -- image orientation by EXIF
+swayimg.dnd_button = "MouseRight"      -- drag-and-drop mouse button
 
 -- Image list configuration
-swayimg.imagelist.set_order("numeric")    -- list order
-swayimg.imagelist.enable_reverse(false)   -- reverse order
-swayimg.imagelist.enable_recursive(false) -- recursive directory reading
-swayimg.imagelist.enable_adjacent(false)  -- add adjacent files from same dir
-swayimg.imagelist.enable_fsmon(true)      -- enable file system monitoring
+swayimg.imagelist.order = "numeric"    -- list order
+swayimg.imagelist.reverse = false   -- reverse order
+swayimg.imagelist.recursive = false -- recursive directory reading
+swayimg.imagelist.adjacent = false  -- add adjacent files from same dir
+swayimg.imagelist.fsmon = true      -- enable file system monitoring
 
 -- Text overlay configuration
-swayimg.text.set_font("monospace")        -- font name
-swayimg.text.set_size(18)                 -- font size in pixels
-swayimg.text.set_spacing(0)               -- line spacing
-swayimg.text.set_padding(10)              -- padding from window edge
-swayimg.text.set_foreground(0xFFebdbb2)   -- foreground text color
-swayimg.text.set_background(0x00000000)   -- text background color
-swayimg.text.set_shadow(0x0d000000)       -- text shadow color
-swayimg.text.set_timeout(5)               -- layer hide timeout
-swayimg.text.set_status_timeout(3)        -- status message hide timeout
+swayimg.text.font = "monospace"        -- font name
+swayimg.text.size = 18                 -- font size in pixels
+swayimg.text.spacing = 0               -- line spacing
+swayimg.text.padding = 10              -- padding from window edge
+swayimg.text.color = 0xFFebdbb2   -- foreground text color
+swayimg.text.background = 0x00000000   -- text background color
+swayimg.text.shadow = 0x0d000000       -- text shadow color
+swayimg.text.timeout = 5               -- layer hide timeout
+swayimg.text.status_timeout = 3        -- status message hide timeout
 
 -- Image viewer mode
-swayimg.viewer.set_default_scale("optimal")      -- default image scale
-swayimg.viewer.set_default_position("center")    -- default image position
-swayimg.viewer.set_drag_button("MouseLeft")      -- mouse button to drag image
+swayimg.viewer.default_scale = "optimal"      -- default image scale
+swayimg.viewer.default_position = "center"    -- default image position
+swayimg.viewer.drag_button = "MouseLeft"      -- mouse button to drag image
 swayimg.viewer.set_window_background(0xFF32302f) -- window background color
 swayimg.viewer.set_image_chessboard(20, 0xff333333, 0xff4c4c4c) -- chessboard
-swayimg.viewer.enable_centering(true)            -- enable automatic centering
-swayimg.viewer.enable_loop(true)                 -- enable image list loop mode
-swayimg.viewer.limit_preload(1)                  -- number of images to preload
-swayimg.viewer.limit_history(1)                  -- number of the history cache
-swayimg.viewer.set_mark_color(0xff808080)        -- mark icon color
-swayimg.viewer.set_pinch_factor(1.0)             -- pinch gesture factor
+swayimg.viewer.autocenter = true            -- enable automatic centering
+swayimg.viewer.loop = true                 -- enable image list loop mode
+swayimg.viewer.preload = 1                  -- number of images to preload
+swayimg.viewer.history = 1                  -- number of the history cache
+swayimg.viewer.mark_color = 0xff808080        -- mark icon color
+swayimg.viewer.pinch_factor = 1.0             -- pinch gesture factor
 swayimg.viewer.set_text("topleft", {             -- top left text block scheme
     "File: {name}",
     "Format: {format}",
@@ -136,27 +137,27 @@ swayimg.viewer.on_key("Shift+r", function()
 end)   
 
 -- Slide show mode, same config as for viewer mode with the following defaults:
-swayimg.slideshow.set_timeout(5)                    -- timeout to switch image
-swayimg.slideshow.set_default_scale("fit")          -- default image scale
+swayimg.slideshow.timeout = 5                    -- timeout to switch image
+swayimg.slideshow.default_scale = "fit"          -- default image scale
 swayimg.slideshow.set_window_background("auto")     -- window background mode
-swayimg.slideshow.limit_history(0)                  -- number of the history cache
+swayimg.slideshow.history = 0                  -- number of the history cache
 swayimg.slideshow.set_text("topleft", { "{name}" }) -- top left text block scheme
 
 
 -- Gallery mode
-swayimg.gallery.set_aspect("fill")                  -- thumbnail aspect ratio
-swayimg.gallery.set_thumb_size(200)                 -- thumbnail size in pixels
-swayimg.gallery.set_padding_size(5)                 -- padding between thumbnails
-swayimg.gallery.set_border_size(2)                  -- border size for selected thumbnail
-swayimg.gallery.set_border_color(0xFFf2e5bc)        -- border color for selected thumbnail
-swayimg.gallery.set_selected_scale(1.05)            -- scale for selected thumbnail
-swayimg.gallery.set_selected_color(0xFFd3869b)      -- background color for selected thumbnail
-swayimg.gallery.set_unselected_color(0xff202020)    -- background color for unselected thumbnail
-swayimg.gallery.set_window_color(0xFF32302f)        -- window background color
-swayimg.gallery.set_pinch_factor(100.0)             -- pinch gesture factor
-swayimg.gallery.limit_cache(100)                    -- number of thumbnails stored in memory
-swayimg.gallery.enable_preload(false)               -- preloading invisible thumbnails
-swayimg.gallery.enable_pstore(false)                -- enable persistent storage for thumbnails
+swayimg.gallery.aspect = "fill"                  -- thumbnail aspect ratio
+swayimg.gallery.thumb_size = 200                 -- thumbnail size in pixels
+swayimg.gallery.padding_size = 5                 -- padding between thumbnails
+swayimg.gallery.border_size = 2                  -- border size for selected thumbnail
+swayimg.gallery.border_color = 0xFFf2e5bc        -- border color for selected thumbnail
+swayimg.gallery.selected_scale = 1.05            -- scale for selected thumbnail
+swayimg.gallery.selected_color = 0xFFd3869b      -- background color for selected thumbnail
+swayimg.gallery.unselected_color = 0xff202020    -- background color for unselected thumbnail
+swayimg.gallery.window_color = 0xFF32302f        -- window background color
+swayimg.gallery.pinch_factor = 100.0             -- pinch gesture factor
+swayimg.gallery.cache = 100                    -- number of thumbnails stored in memory
+swayimg.gallery.preload = false               -- preloading invisible thumbnails
+swayimg.gallery.pstore = false                -- enable persistent storage for thumbnails
 swayimg.gallery.set_text("topleft", {               -- top left text block scheme
     "File: {name}"
 })
